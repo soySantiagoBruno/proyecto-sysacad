83cc660c09ce7d25ae65c664a42b5a60574f8335
Santiago Bruno <soysantiagobruno@gmail.com>
2025-06-18 18:18:02 -0300

    agrego rutas con blueprint para poder usar el certificado

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from app.config import config
 
+
+# Creás la instancia global de la base de datos (se va a vincular con app después).
 db = SQLAlchemy()
 
 def create_app() -> Flask:
@@ -16,10 +18,23 @@ def create_app() -> Flask:
     app = Flask(__name__)
     f = config.factory(app_context if app_context else 'development')
     app.config.from_object(f)
+
+    
+    
+    # Conecta la instancia de SQLAlchemy con tu app Flask.
     db.init_app(app)
 
+
+
+    # Esto es un "atajo" para que, si entrás a la shell (flask shell), tengas acceso a app automáticamente.
     @app.shell_context_processor    
     def ctx():
         return {"app": app}
     
+
+    # Registrar rutas si usás Blueprint
+    from app.routes.certificados import certificados_bp
+    app.register_blueprint(certificados_bp)
+
+
     return app
