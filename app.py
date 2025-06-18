be78e921f749c0a5e28a5e6bbba25bde1f324c48
Santiago Bruno <soysantiagobruno@gmail.com>
2025-06-18 18:13:38 -0300

    limpio archivo principal "app"

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ from app import create_app
 import logging
 # Ref: https://docs.python.org/3/library/logging.html
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+
+
+# funcion en __init__.py que devuelve una instancia de la app Flask.
 app = create_app()
 
-#https://flask.palletsprojects.com/en/stable/appcontext/
-app.app_context().push()
 
 if __name__ == '__main__':
-    """
-    Server Startup
-    Ref: https://flask.palletsprojects.com/en/stable/api/#flask.Flask.run
-    Ref: Book Flask Web Development Page 9
-    """
+    # arranca el servidor Flask localhost:5000
     app.run(host="0.0.0.0", debug=True, port=5000)
     
