ec577445fb3f0be68a736c02893b53621ae8ee80
Santiago Bruno <soysantiagobruno@gmail.com>
2025-08-06 17:47:11 -0300

    actualizo el instalador de dependencias para mostrar un error en caso de que surga

--- install.ps1
+++ install.ps1
@@ -1,9 +1,12 @@
-# Se Necesitan tener los permisos de seguridad configurados con Set-ExecutionPolicy RemoteSigned
-# para ello abrir la consola de PowerShell como administrador y ejecutar:
+# Se necesitan tener los permisos de seguridad configurados con Set-ExecutionPolicy RemoteSigned
+# Para ello, abrir la consola de PowerShell como administrador y ejecutar:
 # Set-ExecutionPolicy RemoteSigned
 
 # Activar el entorno virtual
 # Cambiar la ruta del entorno virtual si es necesario
 & "$env:USERPROFILE\environments\gral_env\Scripts\Activate.ps1"
 
-pip3 install -r requirements.txt
+Write-Host "✅ Entorno virtual 'gral_env' activado correctamente." -ForegroundColor Green
+
+# Instalar dependencias
+pip install -r requirements.txt
